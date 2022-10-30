d4f542d3a46a8229ce4491ef22571e64ff03b34c
Cassandra <umehcassandra@gmail.com>
2022-10-30 19:50:51 +0100

    fix bug

--- src/MyError.js
+++ src/MyError.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./Error.css";
+import { useNavigate } from "react-router-dom";
 
 function MyError() {
+  const navigate = useNavigate();
+  const handleError = () => {
+    navigate("/");
+  };
   return (
     <div className="Errorcontainer">
       <h2>Oops! Something Went Wrong!</h2>
       <p>Check the url and try again.</p>
-      <button className="Home-btn">Back to Home</button>
+      <button onClick={handleError} className="Home-btn">
+        Back to Home
+      </button>
     </div>
   );
 }
